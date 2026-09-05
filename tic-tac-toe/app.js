1d0813c363a4db2d55dc50b734feeb1773bd21dc
vikkikumar07 <vikkiofficial.in@gmail.com>
2026-09-05 22:06:12 +0530

    write html and css code

---
+++ tic-tac-toe/app.js
@@ -0,0 +1,12 @@
+let boxs = document.querySelectorAll(".box");
+let win = document.querySelectorAll(".win");
+let para = document.querySelectorAll("p");
+let newGame = document.querySelectorAll("#new-game");
+let resetGame = document.querySelectorAll("#reset-game");
+
+
+boxs.forEach((box) => {
+    box.addEventListener("click", () => {
+        
+    });
+});
